529b0cd334676faff2f924fa83041e4bc3f34778
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-10-23 18:22:21 +0300

    features: create route and NotFoundPage component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
-import Header from '../Header/Header';
-import NotFaundPage from '../pages/notFaundPage/NotFaundPage';
+import { Routes, Route } from 'react-router-dom'
+import Layout from '../HOC/Layout/Layout';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
-    <>
-      <Header />
-      <NotFaundPage />
-    </>
+    <Routes>
+       <Route path='/' element={<Layout />}>
+          <Route path='/phones' element={<h1>111</h1>} />
+          <Route path='*' element={<NotFoundPage />} />
+       </Route>
+    </Routes>
   );
 }
 
--- src/components/pages/NotFoundPage/NotFoundPage.tsx
+++ src/components/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,13 +1,13 @@
-import './NotFaundPage.scss';
+import './NotFoundPage.scss';
 import React from 'react';
 
-const NotFaundPage: React.FC = () => {
+const NotFoundPage: React.FC = () => {
     return (
         <div className='container'>
           <img src='https://i.imgur.com/nRYqhC5.jpg' alt='error 404' />
-          <p className='notFaundPageTitle'>Page not Found</p>
+          <p className='notFoundPageTitle'>Page not Found</p>
         </div>
     )
 };
 
-export default NotFaundPage;
+export default NotFoundPage;
