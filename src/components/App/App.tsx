5a7874488a4297775f9c6f6faac833df35a5d4f3
Olha Momot <helgammt@gmail.com>
2023-10-24 22:54:00 +0300

    feat: created burger-menu component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,22 @@
 import './App.css';
 import Card from '../Card/Card';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
 import ItemCard from '../ItemCard/ItemCard';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../HOC/Layout/Layout';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
-   <>
-   <Routes>
-    <Route path='/' element={<Layout />}>
-       <Route path='/phones' element={<h1>111</h1>} />
-       <Route path='*' element={<NotFoundPage />} />
-    </Route>
-   </Routes>
- <Header />
- <Card/>
- <Footer />
- <ItemCard />
- </>
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/phones' element={<h1>111</h1>} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      <Card /> 
+      <ItemCard />
+    </>
   );
 }
 
