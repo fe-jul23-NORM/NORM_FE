235baf5afdea8b1b3aecc8b16e294c2c4350e4ba
Tamara Chernetska <cty2802@gmail.com>
2023-10-24 17:00:56 +0300

    fixed: card-component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,14 +9,38 @@ import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 function App() {
   return (
    <>
-   {/* <Routes>
+   <Routes>
     <Route path='/' element={<Layout />}>
        <Route path='/phones' element={<h1>111</h1>} />
        <Route path='*' element={<NotFoundPage />} />
     </Route>
- </Routes> */}
+ </Routes>
  <Card/>
  </>
+  );
+}
+
+export default App;
+
+import './App.css';
+import Header from '../Header/Header';
+import Card from '../Card/Card';
+
+import { Routes, Route } from 'react-router-dom'
+import Layout from '../HOC/Layout/Layout';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/phones' element={<h1>111</h1>} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      <Card />
+    </>
   );
 }
 
