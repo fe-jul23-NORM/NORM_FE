01c21d772dd49c5dbe9defdfea5e172e1f7c5026
Tamara Chernetska <cty2802@gmail.com>
2023-10-24 16:55:59 +0300

    fixed: card-component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,12 +9,12 @@ import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 function App() {
   return (
    <>
-   {/* <Routes>
+   <Routes>
     <Route path='/' element={<Layout />}>
        <Route path='/phones' element={<h1>111</h1>} />
        <Route path='*' element={<NotFoundPage />} />
     </Route>
- </Routes> */}
+ </Routes>
  <Card/>
  </>
   );
