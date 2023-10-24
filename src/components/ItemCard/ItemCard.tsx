db4ac370172552d7fcdff4be22dc4a64f23f6b6a
Tamara Chernetska <cty2802@gmail.com>
2023-10-24 18:34:11 +0300

    created ItemCard

---
+++ src/components/ItemCard/ItemCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ItemCard.scss';
+
+const Card: React.FC = () => {
+  return (
+    <>
+      <div className='item-card'></div>
+    </>
+  )
+};
+
+export default Card;
