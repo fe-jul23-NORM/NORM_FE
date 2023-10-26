9979d941bcf251962ad1b895bd247fe24ca79d55
Yuliia Antonenko <shinjisan311@gmail.com>
2023-10-26 16:34:26 +0300

    commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,40 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from '../HOC/Layout/Layout';
-import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
-import './App.css';
 import Card from '../Card/Card';
 import ItemCard from '../ItemCard/ItemCard';
-// import { Route, Routes } from 'react-router-dom';
-// import Layout from '../HOC/Layout/Layout';
-// import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Layout from '../HOC/Layout/Layout';
 import Catalog from '../Catalog/Catalog';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import { useAppDispatch } from '../../store';
-import { useEffect } from 'react';
-import { getProductsThunk } from '../../store/products/thunks';
-import { ProductTypesEnum } from '../../types/product.types';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
+import HomePage from '../pages/HomePage/HomePage';
+import Cart from '../Cart/Cart';
 
 function App() {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(getProductsThunk({
-      page: 1,
-      perPage: 10,
-      productType: ProductTypesEnum.Phones,
-    }));
-  }, []);
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        {/* <Route path='/' element={<HomePage />} /> */}
+        <Route path='/' element={<HomePage />} />
         <Route path='/phones' element={<Catalog />} />
-        {/* <Route path='/cart' element={<Cart />} /> */}
+        <Route path='/cart' element={<Cart />} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
-
 }
 
 export default App;
