7b8f9f8bcefb34f36be3c85f8d9e072df8cf10c0
Olha Momot <helgammt@gmail.com>
2023-10-26 17:39:32 +0300

    features: redux-test

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,9 +7,31 @@ import Catalog from '../Catalog/Catalog';
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
 import HomePage from '../pages/HomePage/HomePage';
 import Cart from '../Cart/Cart';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../store';
+import { getProductsThunk } from '../../store/products/thunks';
+import { IProduct, ProductTypesEnum } from '../../types/product.types';
+import { addToFavorites } from '../../store/products/slice';
+import { BASE_URI } from '../../constants/core';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const allProducts = useAppSelector((state) => state.product.all);
+
+  const handleClick = (product: IProduct) => {
+    dispatch(addToFavorites(product))
+  }
+
+  useEffect(() => {
+    dispatch(getProductsThunk({
+      page: 2,
+      perPage: 10,
+      productType: ProductTypesEnum.Phones,
+    }))
+  }, [])
+
   return (
+    <>
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route path='/' element={<HomePage />} />
@@ -18,6 +40,17 @@ function App() {
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     </Routes>
+
+    {allProducts.map((product) => {
+      return (
+        <div>
+          <p>{product.name}</p>
+          <img src={`${BASE_URI}/${product.image}`} />
+          <button onClick={() => handleClick(product)}>jopa</button>
+        </div>
+      )
+    })}
+    </>
   );
 }
 
