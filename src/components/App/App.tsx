4ff0306b7bc16d7dbfda7ff60cfb93e35eb65bed
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-10-27 18:45:15 +0300

    commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from '../HOC/Layout/Layout';
 import Catalog from '../Catalog/Catalog';
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
-// import HomePage from '../pages/HomePage/HomePage';
+import HomePage from '../pages/HomePage/HomePage';
 import Cart from '../Cart/Cart';
 import { useAppDispatch, useAppSelector } from '../../store';
 import { getProductsThunk } from '../../store/products/thunks';
@@ -32,7 +32,7 @@ function App() {
   <>
     <Routes>
       <Route path='/' element={<Layout />}>
-        {/* <Route path='/' element={<HomePage />} /> */}
+        <Route path='/' element={<HomePage />} />
         <Route path='/phones' element={<Catalog />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='*' element={<NotFoundPage />} />
