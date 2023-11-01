08edc917f9333f7b986280f924c73c7facd50e25
Yuliia Antonenko <shinjisan311@gmail.com>
2023-11-01 16:49:42 +0200

    fix: fixed page navigation

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -70,7 +70,7 @@ const Card: React.FC<Props> = ({ product }) => {
   }, [user, isFavourite]);
 
   const handleNavigate = () => {
-    navigate(`/${id}`);
+    navigate(`/${itemId}`);
     window.location.reload();
   }
 
