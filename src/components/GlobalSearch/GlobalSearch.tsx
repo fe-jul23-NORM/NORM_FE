ebe9e5bfa22e836d3a4dfec9c7aebf232637ddde
Tamara Chernetska <cty2802@gmail.com>
2023-11-01 14:09:04 +0200

    fix: add memo

--- src/components/GlobalSearch/GlobalSearch.tsx
+++ src/components/GlobalSearch/GlobalSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import './GlobalSearch.scss';
 import { useAppDispatch } from '../../store';
 import { getFoundProductsThunk } from '../../store/products/thunks';
