403186ab5b71bfe86569235553b03c82e9043a52
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-11-02 10:35:19 +0200

    fixed: minor style and code issues;

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,25 @@
-import './Loader.scss';
 import React from 'react';
+import './Loader.scss';
+import { Oval } from 'react-loader-spinner';
 
-export const Loader: React.FC = () => (
+type Props = {
+  size?: number,
+}
+
+export const Loader: React.FC<Props> = ({ size }) => (
   <div className="loader__container">
-    <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+    <Oval
+      height={50 || size}
+      width={50 || size}
+      color="#F86800"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel='oval-loading'
+      secondaryColor="#cfd2d5"
+      strokeWidth={2}
+      strokeWidthSecondary={2}
+    
+    />
   </div>
 );
